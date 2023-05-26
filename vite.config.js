43e505ff708037da6c051c8285f9b62cfd385f74
Opeoluwa-Ogunlaja <ogunlajafaith@gmail.com>
2023-05-26 23:31:37 +0100

    updated base url

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/event-planning-site/"
+  base: "/event_planning_site/"
 })
